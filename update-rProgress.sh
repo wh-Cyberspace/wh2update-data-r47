d231ec882b6f032adb23115e98f37a9de39f137a
wh-hackerexploit <47976175+wh-hackerexploit@users.noreply.github.com>
2019-12-29 15:18:05 -0500

    Update update-rProgress.sh

--- update-rProgress.sh
+++ update-rProgress.sh
@@ -23,10 +23,32 @@ echo "${RESTORE}"
 
 sleep 1.5;
 
-echo "${GREEN}[*]${YELLOW} YOU ARE USING LATEST VERSION : ${GREEN}V-2.0.1 .${RESTORE}"
+echo "${GREEN}[*]${YELLOW} Framwrok update available VERSION : ${GREEN}V-2.0.2 .${RESTORE}"
 
 echo ""
 echo ""
+
+
+
+
+#-----------------------------------------
+
+rm -rf $LPARTH/actions/ngwindows-ipia.sh
+rm -rf $LPARTH/actions/windows-ipia.sh
+rm -rf $LPARTH/toolinfo
+sleep 1;
+mv $LPARTH/wh2update-data-r47/ngwindows-ipia_up1.sh $LPARTH/actions/ngwindows-ipia.sh
+mv $LPARTH/wh2update-data-r47/windows-ipia1_up1.sh $LPARTH/actions/windows-ipia.sh
+mv $LPARTH/wh2update-data-r47/toolinfo $LPARTH/toolinfo
+
+#-----------------------------------------
+
+
+
+
+
+
+
 
 echo "${YELLOW}[*] Press enter to Run WH2/(WH-HackerExploit v2) framework ${GREEN}V-2.0.1 .${RESTORE}"
 read whenter
